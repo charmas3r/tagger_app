e1df40af5e1686349dfa19e1bf2df8b9d89c8fd4
Evan Smith <evansmith0115@gmail.com>
2022-11-12 20:55:32 -0800

    adds overlay for camera qr scanning

--- lib/qr/presentation/pages/qr_scanning_page.dart
+++ lib/qr/presentation/pages/qr_scanning_page.dart
@@ -20,16 +20,32 @@ class _QRScanningPage extends State<QRScanningPage> {
           .of(context)
           .size
           .height,
-      child: MobileScanner(
-          allowDuplicates: false,
-          onDetect: (barcode, args) {
-            if (barcode.rawValue != null) {
-              const snackBar = SnackBar(
-                  content: Text('Successfully scanned barcode!')
-              );
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            }
-          }),
+      child: Stack(
+        children: [
+          MobileScanner(
+              allowDuplicates: false,
+              onDetect: (barcode, args) {
+                if (barcode.rawValue != null) {
+                  const snackBar = SnackBar(
+                      content: Text('Successfully scanned barcode!')
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+              }),
+          //overlays a semi-transparent rounded square border that is 60% of screen width
+          Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              height: MediaQuery.of(context).size.width * 0.6,
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(color: Colors.white38, width: 4.0),
+                borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+              ),
+            ),
+          ),
+        ],
+      )
     );
   }
 }
